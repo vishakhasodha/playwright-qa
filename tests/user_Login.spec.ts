182fbb1303d85f13639996a1f18943889284cec7
VISHAKHA SODHA <vishakha.sodha@smartstream-stp.com>
2026-03-12 14:09:19 +0530

    playwright files

---
+++ tests/user_Login.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Register user', async ({ page }) => {
+  await page.goto('https://automationexercise.com/');
+
+  // Expect a title "to contain" a substring.
+    const title = await page.title();
+  console.log(title);
+    await expect(page).toHaveTitle(title); // Assert the page title
+await page.locator('.fa-lock').click();
+// await page.locator('.signup-form').textContent 
+await expect(page.locator('.signup-form h2')).toHaveText('New User Signup!');
+
+const new_username='vishakha';
+const new_email='vishakha@gmail.com';
+const password='newpassword';
+const dob='09/02/1992';
+
+// Parsing DOB and passing in selection locator
+
+ const parts = dob.split('/'); // [month, day, year]
+
+  if (parts.length !== 3) {
+    throw new Error('Invalid input format, expected mm/dd/yyyy');
+  }
+
+  // Extract month, day, and the last two digits of the year
+  const month = parts[0];
+  const day = parts[1];
+  const year_yy = parts[2].slice(-2);
+
+
+
+await page.locator("//input[@data-qa='signup-name']").fill(new_username);
+
+await page.locator("//input[@data-qa='signup-email']").fill(new_email);
+ await page.locator("//button[@data-qa='signup-button']").click();
+
+await expect (page.locator("//input[@data-qa='name']")).toHaveValue('new_username');
+await expect (page.locator("//input[@data-qa='email']")).toHaveValue('new_email');
+await page.locator("//input[@id='id_gender2']").check();
+await page.locator("//input[@data-qa='email']").fill('password');
+await page.locator("//*[@data-qa='days']").getByRole('option', { name: day }).click(); 
+// Example for a custom dropdown component
+
+
+
+
+//   console.log(page.title());
+//   await expect(page).toHaveTitle(/Playwright/);
+});
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
